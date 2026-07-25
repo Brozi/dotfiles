d77d3f56d3851d15d1348d23595f525e7f468438
Brozi <jasiubar@gmail.com>
2026-07-26 00:28:54 +0200

    Update autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -21,38 +21,44 @@
 --     vim.w.gotmpl_match_id = vim.fn.matchadd("Special", "{{.\\{-}}}")
 --   end,
 -- })
+local is_syncing = false
+
 vim.api.nvim_create_autocmd("BufWritePost", {
-  group = vim.api.nvim_create_augroup("ChezmoiAsyncCommit", { clear = true }),
+  group = vim.api.nvim_create_augroup("ChezmoiSafeAsyncCommit", { clear = true }),
   pattern = {
     vim.env.HOME .. "/.local/share/chezmoi/*",
     vim.env.HOME .. "/.local/share/chezmoi/**/*",
   },
   callback = function(args)
+    if is_syncing then
+      return
+    end
+    is_syncing = true
+
     local filepath = args.match
     local filename = vim.fn.fnamemodify(filepath, ":t")
-    local msg = "Update " .. filename
+    local msg = "Auto-commit: Update " .. filename
 
-    -- 1. Apply changes asynchronously
     vim.system({ "chezmoi", "apply", "--source-path", filepath }, {}, function(apply_out)
       if apply_out.code ~= 0 then
+        is_syncing = false
         return
       end
-      -- 2. Stage the file
+
       vim.system({ "chezmoi", "git", "--", "add", filepath }, {}, function(add_out)
         if add_out.code ~= 0 then
+          is_syncing = false
           return
         end
-        -- 3. Commit the file
+
         vim.system({ "chezmoi", "git", "--", "commit", "-m", msg }, {}, function(commit_out)
           if commit_out.code ~= 0 then
+            is_syncing = false
             return
           end
-          -- Notify the user that the push is starting
-          vim.schedule(function()
-            vim.notify("Chezmoi: Pushing " .. filename .. " in background...", vim.log.levels.INFO)
-          end)
-          -- 4. Push to remote
+
           vim.system({ "chezmoi", "git", "--", "push" }, {}, function(push_out)
+            is_syncing = false
             vim.schedule(function()
               if push_out.code == 0 then
                 vim.notify("Chezmoi: Successfully pushed " .. filename, vim.log.levels.INFO)
