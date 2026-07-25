03e22e290df1ac11911e3637a541f94c968abeed
Brozi <jasiubar@gmail.com>
2026-07-25 22:57:54 +0200

    Auto-commit: Update autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -21,3 +21,48 @@
 --     vim.w.gotmpl_match_id = vim.fn.matchadd("Special", "{{.\\{-}}}")
 --   end,
 -- })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = vim.api.nvim_create_augroup("ChezmoiAsyncCommit", { clear = true }),
+  pattern = {
+    vim.env.HOME .. "/.local/share/chezmoi/*",
+    vim.env.HOME .. "/.local/share/chezmoi/**/*",
+  },
+  callback = function(args)
+    local filepath = args.match
+    local filename = vim.fn.fnamemodify(filepath, ":t")
+    local msg = "Update " .. filename
+
+    -- 1. Apply changes asynchronously
+    vim.system({ "chezmoi", "apply", "--source-path", filepath }, {}, function(apply_out)
+      if apply_out.code ~= 0 then
+        return
+      end
+      -- 2. Stage the file
+      vim.system({ "chezmoi", "git", "--", "add", filepath }, {}, function(add_out)
+        if add_out.code ~= 0 then
+          return
+        end
+        -- 3. Commit the file
+        vim.system({ "chezmoi", "git", "--", "commit", "-m", msg }, {}, function(commit_out)
+          if commit_out.code ~= 0 then
+            return
+          end
+          -- Notify the user that the push is starting
+          vim.schedule(function()
+            vim.notify("Chezmoi: Pushing " .. filename .. " in background...", vim.log.levels.INFO)
+          end)
+          -- 4. Push to remote
+          vim.system({ "chezmoi", "git", "--", "push" }, {}, function(push_out)
+            vim.schedule(function()
+              if push_out.code == 0 then
+                vim.notify("Chezmoi: Successfully pushed " .. filename, vim.log.levels.INFO)
+              else
+                vim.notify("Chezmoi: Push failed for " .. filename, vim.log.levels.ERROR)
+              end
+            end)
+          end)
+        end)
+      end)
+    end)
+  end,
+})
