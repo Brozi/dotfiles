ca409e9e594f562415b48386891f98a69191035b
Brozi <jasiubar@gmail.com>
2026-07-28 02:51:43 +0200

    Update nvim-spider.lua

--- dot_config/nvim/lua/plugins/nvim-spider.lua
+++ dot_config/nvim/lua/plugins/nvim-spider.lua
@@ -1,24 +1,24 @@
--- return {
---   "chrisgrieser/nvim-spider",
---   opts = {},
---   keys = {
---     {
---       "w",
---       "<cmd>lua require('spider').motion('w')<CR>",
---       mode = { "n", "o", "x" },
---       desc = "Move to start of the next word",
---     },
---     {
---       "e",
---       "<cmd>lua require('spider').motion('e')<CR>",
---       mode = { "n", "o", "x" },
---       desc = "Move to the end of the word",
---     },
---     {
---       "b",
---       "<cmd>lua require('spider').motion('b')<CR>",
---       mode = { "n", "o", "x" },
---       desc = "Move to the start of the previous word",
---     },
---   },
--- }
+return {
+  "chrisgrieser/nvim-spider",
+  opts = {},
+  keys = {
+    {
+      "w",
+      "<cmd>lua require('spider').motion('w')<CR>",
+      mode = { "n", "o", "x" },
+      desc = "Move to start of the next word",
+    },
+    {
+      "e",
+      "<cmd>lua require('spider').motion('e')<CR>",
+      mode = { "n", "o", "x" },
+      desc = "Move to the end of the word",
+    },
+    {
+      "b",
+      "<cmd>lua require('spider').motion('b')<CR>",
+      mode = { "n", "o", "x" },
+      desc = "Move to the start of the previous word",
+    },
+  },
+}
