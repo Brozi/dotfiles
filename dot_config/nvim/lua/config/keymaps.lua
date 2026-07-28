db5fd97f4fdd4668527a4bf78c5298c174dc53a8
Brozi <jasiubar@gmail.com>
2026-07-28 23:15:31 +0200

    Update keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,7 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", ";;", "<ESC>", { noremap = false })
-
--- Bind ce to cw in order to preserve cw's default behaviour
--- While using nvim spider
-vim.keymap.set("n", "cw", "c<cmd>lua require('spider').motion('e')<CR>")
