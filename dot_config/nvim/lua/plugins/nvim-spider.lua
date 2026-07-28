f23ce73e983cd28c8517dcaee202c3c5b8d108b0
Brozi <jasiubar@gmail.com>
2026-07-28 23:10:15 +0200

    Update nvim-spider.lua

--- dot_config/nvim/lua/plugins/nvim-spider.lua
+++ dot_config/nvim/lua/plugins/nvim-spider.lua
@@ -9,19 +9,19 @@ return {
     {
       "w",
       "<cmd>lua require('spider').motion('w')<CR>",
-      mode = { "n", "x" },
+      mode = { "n", "o", "x" },
       desc = "Move to start of the next word",
     },
     {
       "e",
       "<cmd>lua require('spider').motion('e')<CR>",
-      mode = { "n", "x" },
+      mode = { "n", "o", "x" },
       desc = "Move to the end of the word",
     },
     {
       "b",
       "<cmd>lua require('spider').motion('b')<CR>",
-      mode = { "n", "x" },
+      mode = { "n", "o", "x" },
       desc = "Move to the start of the previous word",
     },
   },
