962c6d5e8a3300f86c84f665c0fa3c06f04a4615
Brozi <jasiubar@gmail.com>
2026-08-25 00:24:39 +0200

    add spellcheck language switch

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", ";;", "<ESC>", { noremap = false })
+
+-- Cycle spelllang between English and Polish
+vim.keymap.set("n", "<leader>uz", function()
+  local current_lang = vim.bo.spelllang
+  if current_lang == "en_us" then
+    vim.opt_local.spelllang = "pl"
+    vim.notify("Spellcheck: Polish")
+  else
+    vim.opt_local.spelllang = "en_us"
+    vim.notify("Spellcheck: English")
+  end
+end, { desc = "Toggle Spell Language" })
