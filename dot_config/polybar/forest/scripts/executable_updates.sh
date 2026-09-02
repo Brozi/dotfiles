bbf1836913868138da5ed84f1965377a74853fc6
Brozi <jasiubar@gmail.com>
2026-09-02 14:00:24 +0200

    Update .config/polybar/forest/scripts/updates.sh

--- dot_config/polybar/forest/scripts/executable_updates.sh
+++ dot_config/polybar/forest/scripts/executable_updates.sh
@@ -2,44 +2,55 @@
 
 NOTIFY_ICON=/usr/share/icons/Papirus/32x32/apps/system-software-update.svg
 
-get_total_updates() { UPDATES=$(~/.config/polybar/forest/scripts/updates-apt 2>/dev/null | wc -l); }
+get_mint_interval_seconds() {
+  # Fetch values and strictly strip out any non-numeric characters (like "uint32 ")
+  local mins=$(gsettings get org.linuxmint.updates refresh-minutes 2>/dev/null | tr -cd '0-9')
+  local hours=$(gsettings get org.linuxmint.updates refresh-hours 2>/dev/null | tr -cd '0-9')
+  local days=$(gsettings get org.linuxmint.updates refresh-days 2>/dev/null | tr -cd '0-9')
+
+  # Enforce default integer fallbacks if the variables end up empty
+  mins=${mins:-0}
+  hours=${hours:-2}
+  days=${days:-0}
+
+  # Calculate total interval in seconds
+  echo $(((days * 86400) + (hours * 3600) + (mins * 60)))
+}
+
+get_total_updates() {
+  UPDATES=$(~/.config/polybar/forest/scripts/updates-apt 2>/dev/null | wc -l)
+}
 
 while true; do
   get_total_updates
+  REFRESH_SEC=$(get_mint_interval_seconds)
 
-  # notify user of updates
+  # Validate that REFRESH_SEC is actually a number, and enforce the 10-minute minimum
+  if ! [[ "$REFRESH_SEC" =~ ^[0-9]+$ ]] || ((REFRESH_SEC < 600)); then
+    REFRESH_SEC=3600
+  fi
+
+  # Notify user of updates
   if hash notify-send &>/dev/null; then
     if ((UPDATES > 20)); then
-      notify-send -u critical -i $NOTIFY_ICON \
+      notify-send -u critical -i "$NOTIFY_ICON" \
         "You really need to update!!" "$UPDATES New packages"
     elif ((UPDATES > 10)); then
-      notify-send -u normal -i $NOTIFY_ICON \
+      notify-send -u normal -i "$NOTIFY_ICON" \
         "You should update soon" "$UPDATES New packages"
     elif ((UPDATES > 2)); then
-      notify-send -u low -i $NOTIFY_ICON \
+      notify-send -u low -i "$NOTIFY_ICON" \
         "$UPDATES New packages"
     fi
   fi
 
-  # when there are updates available
-  # every 10 seconds another check for updates is done
-  while ((UPDATES > 0)); do
-    if ((UPDATES == 1)); then
-      echo "$UPDATES"
-    elif ((UPDATES > 1)); then
-      echo "$UPDATES"
-    else
-      echo "None"
-    fi
-    sleep 10
-    get_total_updates
-  done
-
-  # when no updates are available, use a longer loop, this saves on CPU
-  # and network uptime, only checking once every 30 min for new updates
-  while ((UPDATES == 0)); do
+  # Output to Polybar
+  if ((UPDATES > 0)); then
+    echo "$UPDATES"
+  else
     echo "None"
-    sleep 1800
-    get_total_updates
-  done
+  fi
+
+  # Wait for the exact duration
+  sleep "$REFRESH_SEC"
 done
