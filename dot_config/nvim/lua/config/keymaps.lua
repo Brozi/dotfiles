22f1dd12bb254add1d30333871240d405b6c4a60
Brozi <jasiubar@gmail.com>
2026-09-02 16:09:30 +0200

    Update keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,7 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.api.nvim_set_keymap("i", ";;", "<ESC>", { noremap = false })
+vim.api.nvim_set_keymap("i", "jj", "<ESC>", { noremap = false })
+vim.api.nvim_set_keymap("i", "jk", "<ESC>", { noremap = false })
 
 -- Cycle spelllang between English and Polish
 vim.keymap.set("n", "<leader>uy", function()
