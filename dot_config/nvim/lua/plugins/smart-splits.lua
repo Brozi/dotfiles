a1d73f72a2fa939e5e8d8838f89100806fd3349c
Brozi <jasiubar@gmail.com>
2026-09-18 22:21:58 +0200

    Update .config/nvim/lua/plugins/smart-splits.lua

--- dot_config/nvim/lua/plugins/smart-splits.lua
+++ dot_config/nvim/lua/plugins/smart-splits.lua
@@ -3,6 +3,7 @@ return {
   build = "./kitty/install-kittens.bash",
   lazy = false,
   keys = {
+    -- Navigation
     {
       "<C-h>",
       function()
@@ -11,25 +12,54 @@ return {
       desc = "Move to left window",
     },
     {
-      "<C-l>",
-      function()
-        require("smart-splits").move_cursor_right()
-      end,
-      desc = "Move to right window",
-    },
-    {
       "<C-j>",
       function()
         require("smart-splits").move_cursor_down()
       end,
-      desc = "Move to below window",
+      desc = "Move to lower window",
     },
     {
       "<C-k>",
       function()
         require("smart-splits").move_cursor_up()
       end,
-      desc = "Move to above window",
+      desc = "Move to upper window",
+    },
+    {
+      "<C-l>",
+      function()
+        require("smart-splits").move_cursor_right()
+      end,
+      desc = "Move to right window",
+    },
+    -- Resizing
+    {
+      "<A-h>",
+      function()
+        require("smart-splits").resize_left()
+      end,
+      desc = "Resize window left",
+    },
+    {
+      "<A-j>",
+      function()
+        require("smart-splits").resize_down()
+      end,
+      desc = "Resize window down",
+    },
+    {
+      "<A-k>",
+      function()
+        require("smart-splits").resize_up()
+      end,
+      desc = "Resize window up",
+    },
+    {
+      "<A-l>",
+      function()
+        require("smart-splits").resize_right()
+      end,
+      desc = "Resize window right",
     },
   },
 }
