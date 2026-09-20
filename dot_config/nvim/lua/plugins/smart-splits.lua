49796895636008aa3de47156c464bdda572e472f
Brozi <jasiubar@gmail.com>
2026-09-20 18:57:55 +0200

    Update smart-splits.lua

--- dot_config/nvim/lua/plugins/smart-splits.lua
+++ dot_config/nvim/lua/plugins/smart-splits.lua
@@ -3,34 +3,64 @@ return {
   build = "./kitty/install-kittens.bash",
   lazy = false,
   keys = {
-    -- Navigation
+    -- Tmux navigation bindings (Ctrl + hjkl)
     {
       "<C-h>",
       function()
         require("smart-splits").move_cursor_left()
       end,
-      desc = "Move to left window",
+      desc = "Move to left split (Tmux)",
     },
     {
       "<C-j>",
       function()
         require("smart-splits").move_cursor_down()
       end,
-      desc = "Move to lower window",
+      desc = "Move to lower split (Tmux)",
     },
     {
       "<C-k>",
       function()
         require("smart-splits").move_cursor_up()
       end,
-      desc = "Move to upper window",
+      desc = "Move to upper split (Tmux)",
     },
     {
       "<C-l>",
       function()
         require("smart-splits").move_cursor_right()
       end,
-      desc = "Move to right window",
+      desc = "Move to right split (Tmux)",
+    },
+
+    -- Kitty native navigation bindings (Alt + hjkl)
+    {
+      "<A-h>",
+      function()
+        require("smart-splits").move_cursor_left()
+      end,
+      desc = "Move to left window (Kitty)",
+    },
+    {
+      "<A-j>",
+      function()
+        require("smart-splits").move_cursor_down()
+      end,
+      desc = "Move to lower window (Kitty)",
+    },
+    {
+      "<A-k>",
+      function()
+        require("smart-splits").move_cursor_up()
+      end,
+      desc = "Move to upper window (Kitty)",
+    },
+    {
+      "<A-l>",
+      function()
+        require("smart-splits").move_cursor_right()
+      end,
+      desc = "Move to right window (Kitty)",
     },
   },
 }
